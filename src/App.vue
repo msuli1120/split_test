a7d9eafebf89694d84460e2bfbd2cabbd304743a
Xing Li <msuli_1120@ymail.com>
2017-06-14 09:53:23 -0700

    split test

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <div class="col-md-6">
+      <h1>App Component</h1>
+      <ul>
+        <li v-for="(todo, index) in todos" @click="remove(index, todo)">{{ todo }}</li>
+      </ul>
+    </div>
+
+    <div class="col-md-6">
+      <p v-for="(thing, index) in output" @click="reverseRemove(index, thing)">{{ thing.todo }}: {{ thing.price }}</p>
+      <div v-if="result != 0">
+        <p v-model="result">Total: {{ result }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      todos: [
+        { todo: 'Do the dishes', price: 1},
+        { todo: 'Take out the trash', price: 5},
+        { todo: 'Movie', price: 3},
+        { todo: 'Sleep', price: 4},
+        { todo: 'Shopping', price: 9},
+      ],
+      output: [],
+      result: 0,
+    }
+  },
+  methods: {
+    remove: function(i, obj){
+      this.output.push(obj);
+      this.todos.splice(i, 1);
+      this.result += obj.price;
+    },
+    reverseRemove: function(i, obj){
+      this.todos.push(obj);
+      this.output.splice(i, 1);
+      this.result -= obj.price;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
